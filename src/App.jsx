97bb3339b140895c30d01444e7376db072e30995
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-01 13:03:45 +0100

    setup routing and base context

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
 
+import React, { createContext, useEffect, useState } from "react";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Team from './pages/Team';
+import Match from './pages/Match';
+import { loadData } from './utils/dataLoader'
 
 
-import './App.css'
+export const  DataContext = createContext();
+
+
+
 
 function App() {
-  
+  const [data, setData] = useState({ teams: [], players: [], matches: [], records: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const loadedData = await loadData();
+      setData(loadedData);
+  };
+  fetchData();
+}, []);
 
   return (
-    <>
-      
-    </>
-  )
+    <DataContext.Provider value={data}>
+      <Router>
+        <div className="App">
+          {/* <Header /> */}
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/match/:id" element={<Match />} />
+            <Route path="/team/:id" element={<Team />} />
+          </Routes>
+          {/* <Footer />*/}
+        </div>
+      </Router>
+    </DataContext.Provider>
+  );
 }
 
 export default App
